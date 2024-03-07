baded5b5fab684eb2ffbdacb410cca5626034cc9
xuliwenwenwen <liwen.xu@pingcap.com>
2024-03-07 11:06:30 +0800

    feat: serverless jp conf

--- mysql/init/01.sql
+++ mysql/init/01.sql
@@ -1,4 +1,5 @@
 # create databases
 CREATE DATABASE IF NOT EXISTS `pingcap_website_en`;
 CREATE DATABASE IF NOT EXISTS `pingcap_website_cn`;
+CREATE DATABASE IF NOT EXISTS `pingcap_website_jp`;
 GRANT ALL PRIVILEGES ON *.* TO 'pingcap'@'%';
